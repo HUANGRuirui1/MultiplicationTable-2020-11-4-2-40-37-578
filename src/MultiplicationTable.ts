6854af86c64e9f9f859acd661934310cb864abd4
Ruirui Huang <rhuang2@slb.com>
2020-11-17 15:03:51 +0800

    feat:  AC1 and AC2

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,20 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    const expressions  = ''
+    if (!this.isValidRange(start, end)){
+      return ''
+    }
+    if (!this.isValidScope(start, end)){
+      return 'invalid scope!'
+    }
+    return expressions
+  }
+
+  private isValidRange(start : number, end : number){
+    return start <= end
+  }
+
+  private isValidScope(start : number, end : number){
+    return !((start < 1 || start > 10) || (end < 1 || end > 10))
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,16 +1,25 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
-  it('should render multiplication table of (1, 1)', () => {
+  it('should render table return invalid message if the input numbers is out of scope', () => {
     // given
     const table = new MultiplicationTable()
-    const start = 1
-    const end = 1
-
+    const start = 0
+    const end = 11
     //when
     const rendered = table.render(start, end)
-
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBe('invalid scope!')
+  })
+
+  it('should render table of nothing if the start number is greater than the end number', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 6
+    const end = 2
+    //when
+    const rendered = table.render(start, end)
+    //then
+    expect(rendered).toBe('')
   })
 })
